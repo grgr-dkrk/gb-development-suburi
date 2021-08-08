73faa9576690a0d922bdc2e687d8f9abee6128cf
grgr-dkrk <40130327+grgr-dkrk@users.noreply.github.com>
2021-08-08 18:55:20 +0900

    initial

---
+++ 01-sprite/src/constants.h
@@ -0,0 +1,15 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define MAX_X 160
+#define MAX_Y 144
+
+#define GRID_MAX_X 20
+#define GRID_MAX_Y 18
+
+#define SPRITE_NUM_PLAYER 0
+#define SPRITE_NUM_FRIEND 1
+
+#define SPRITE_SIZE 8;
+
+#endif
